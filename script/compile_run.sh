85b8ba4edf901fee1511f53ea5e12db3c2c9f0d3
chi0819 <lintzuchi31@gmail.com>
2023-10-07 09:59:39 +0000

    new

--- script/compile_run.sh
+++ script/compile_run.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 # Program: For auto compile and run different programs
 
-printf 'Compile and run option \n'
-printf '(1) c \n'
-printf '(2) cpp \n'
-printf '(3) python \n'
-printf '(4) rust \n'
-read -p 'Enter your filename: ' filename
-read -p 'Enter your option: ' option
+filetype=$(find . -type f -name "${1}*" | cut -d '.' -f 3)
 
-if [ ${option} -eq 1 ]; then
-	gcc \-o a ${filename}.c
-elif [ ${option} -eq 2 ]; then
-	g++ \-o a ${filename}.cpp
+if [ ${filetype} == 'c' ]; then
+	gcc \-o a ${1}.c
+elif [ ${filetype} == 'cpp' ]; then
+	g++ \-o a ${1}.cpp
 fi
 
 if [ $? -eq 0 ]; then
-	printf 'Compile finished\nPlease enter your test case: \n'
+	printf "Compile finished and run\n"
+	printf "Enter your test data: \n"
 	./a
 elif [ $? -ne 0 ]; then
-	printf 'Something error !\n'
+	printf "Something error \n"
 fi
-
