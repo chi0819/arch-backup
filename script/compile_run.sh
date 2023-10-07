ef0a338d422a4a85ccc240c75def6713776d09cb
chi0819 <lintzuchi31@gmail.com>
2023-10-07 13:52:06 +0000

    script_debug

--- script/compile_run.sh
+++ script/compile_run.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-# Program: For auto compile and run different programs
+# Program: For compiling and run different programs
 
 filetype=$(find . -type f -name "${1}*" | cut -d '.' -f 3)
 
 interpreter=0
 
-if [ ${filetype} == 'c' ]; then
-	gcc \-o a ${1}.c
-elif [ ${filetype} == 'cpp' ]; then
-	g++ \-o a ${1}.cpp
-elif [ ${filetype} == 'rs' ]; then
-	rustc ${1}.rs
-elif [ ${filetype} == 'py' ]; then
-	python ${1}.py
+if [[ "${filetype}" == "cpp" ]]; then
+	g++ -o "${1}" "${1}.${filetype}"
+elif [[ "${filetype}" == "c" ]]; then
+	gcc -o "${1}" "${1}.${filetype}"
+elif [[ "${filetype}" == "rs" ]]; then
+	rustc "${1}.${filetype}"
+elif [[ "${filetype}" == "py" ]]; then
+	python "${1}.${filetype}"
 	interpreter=1
 fi
 
-if [ ${interpreter} -eq 1 ]; then
+if [[ ${interpreter} -eq 1 ]]; then
 	printf "Compile finished and run\n"
 	printf "Enter your test data: \n\n"
-elif [ $? -eq 0 ]; then
+elif [[ ${interpreter} -eq 0 ]]; then
 	printf "Compile finished and run\n"
 	printf "Enter your test data: \n\n"
-	./a
-elif [ $? -ne 0 ]; then
+	./"${1}"
+elif [[ $? -ne 0 ]]; then
 	printf "Something error \n"
 fi
---
+++ script/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Program: For testing some functions
+
+interpreter=1
+printf "${interpreter}"
+interpreter=0
+printf "${interpreter}"
