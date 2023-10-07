4b97173858d4b997d7fa355f1321a29934fb1ca5
chi0819 <lintzuchi31@gmail.com>
2023-10-07 10:48:13 +0000

    new

--- script/compile_run.sh
+++ script/compile_run.sh
@@ -3,15 +3,25 @@
 
 filetype=$(find . -type f -name "${1}*" | cut -d '.' -f 3)
 
+interpreter=0
+
 if [ ${filetype} == 'c' ]; then
 	gcc \-o a ${1}.c
 elif [ ${filetype} == 'cpp' ]; then
 	g++ \-o a ${1}.cpp
+elif [ ${filetype} == 'rs' ]; then
+	rustc ${1}.rs
+elif [ ${filetype} == 'py' ]; then
+	python ${1}.py
+	interpreter=1
 fi
 
-if [ $? -eq 0 ]; then
+if [ ${interpreter} -eq 1 ]; then
 	printf "Compile finished and run\n"
-	printf "Enter your test data: \n"
+	printf "Enter your test data: \n\n"
+elif [ $? -eq 0 ]; then
+	printf "Compile finished and run\n"
+	printf "Enter your test data: \n\n"
 	./a
 elif [ $? -ne 0 ]; then
 	printf "Something error \n"
